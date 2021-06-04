f288abd79f8b1708a217d9df3422f81f5eed413f
ahmadou5 <78315869+ahmadou5@users.noreply.github.com>
2021-06-04 21:37:14 +0100

    Update Oreo Token.sol
    Token symbol updated from oreo to OREO

--- Contract/Oreo Token.sol
+++ Contract/Oreo Token.sol
@@ -3,7 +3,7 @@ pragma solidity 0.6.12;
 import "@oreoswap/oreo-swap-lib/contracts/token/BEP20/BEP20.sol";
 
 // OreoToken with Governance.
-contract OreoToken is BEP20('OreoSwap Token', 'Oreo') {
+contract OreoToken is BEP20('OreoSwap Token', 'OREO') {
     /// @notice Creates `_amount` token to `_to`. Must only be called by the owner (MasterChef).
     function mint(address _to, uint256 _amount) public onlyOwner {
         _mint(_to, _amount);
