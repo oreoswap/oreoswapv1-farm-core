15c0e22e6f2a8afcd7773db10310475dbba5b0fb
bobeu <35783747+bobeu@users.noreply.github.com>
2021-09-21 05:52:10 -0700

    Updated (#35)
    Co-authored-by: Quatre.Finance <87430168+QuatreFinance@users.noreply.github.com>

--- migrations/2_initial_migration.js
+++ migrations/2_initial_migration.js
@@ -1,3 +1,4 @@
+const Web3 = require("web3");
 const OreoToken = artifacts.require("OreoToken");
 const MilkBar = artifacts.require("MilkBar");
 const PastryChef = artifacts.require("PastryChef");
@@ -5,21 +6,42 @@ const SousChef = artifacts.require("SousChef");
 
 module.exports = async function(deployer) {
 
-    const token = await deployer.deploy(OreoToken);
-    await token.deployed();
+    await deployer.deploy(OreoToken);
+    const token = await OreoToken.deployed();
+    // const token = await OreoToken.new();
     console.log("Oreotoken Address: ", token.address);
 
-    const milk = await deployer.deploy(MilkBar, token.address);
-    await milk.deployed();
+    await deployer.deploy(MilkBar, token.address);
+    const milk = await MilkBar.deployed
+    // const milk = await MilkBar.new(token.address);
     console.log("MilkBar Address: ", milk.address);
 
-    const pastry = await deployer.deploy(PastryChef, token.address, milk.address, '0x146BB7a8F1572784610Ba8964150ac0B87a160F1', 1000, 12474143);
-    await pastry.deployed();
+    await deployer.deploy(PastryChef, token.address, milk.address, '0x146BB7a8F1572784610Ba8964150ac0B87a160F1', 1000, 12474143);
+    const pastry = await PastryChef.deployed();
+    // const pastry = await PastryChef.new(token.address, milk.address, '0x146BB7a8F1572784610Ba8964150ac0B87a160F1', 1000, 12474143);
     console.log("Pastry Address: ", pastry.address);
 
-    const sous = await deployer.deploy(SousChef);
-    await sous.deployed();
-    console.log("Sousbar Address: ", sous.address); 
+    await deployer.deploy(SousChef, milk.address, 1000, 12474143, 12475143);
+    await SousChef.deployed();
+    // const sous = await SousChef.new(milk.address, 1000, 12474143, 12475143);
+    // contract("SOUCHEF", accounts => {
+    //     let sou
+    //     Before( async () => {
+    //         sou = await SousChef.new(milk.address, 1000, 12474143, 12475143);
+    //     });
+
+        
+
+    //     it(
+
+    //     );
+    // }
+
+    // );
+    // console.log("Sousbar Address: ", sous.address); 
+    // console.log("Others: ", Web3.utils.BN(await sous._now.call()).toNumber()); 
+    // console.log("Starttime: ", Web3.utils.BN(await sous.starttime.call()).toNumber()); 
+    // console.log("Sousbar Address: ", sous.address); 
 
 
 };
